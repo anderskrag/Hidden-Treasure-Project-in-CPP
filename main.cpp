691c5ad8554b22bd1471ed3372cb5e1bad05d4b4
anderskrag <anderskrag03@gmail.com>
2026-04-12 20:01:39 +0200

    Takk og farvel. Zip created

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-#include "GameWorld.h"
-#include "TreasureWindow.h"
 #include "HiddenTreasure.h"
 
 int main(){
     HiddenTreasure my_game("init_world.txt", 1, 0.05, 0.03, 0.2, 1); //Skal være 1, 0.05, 0.03, 0.2, 1
     my_game.run();
-
     return 0;
 }
